f49d15093381be2e6b057e3075f5cb1d13191a94
Saurabh Pathak <4saurabhpathak@gmail.com>
2022-02-18 18:21:17 +0530

    My Profile

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,121 @@
+/*import { useState } from "react";
+import "./contact.scss";
+import {db} from "../../firebase";
+
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
+        </form>
+      </div>
+    </div>
+  );
+}*/
+
+
+
+import {useState} from "react";
+import "./contact.scss";
+import {db} from "../../fire";
+
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [loader, setLoader] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoader(true);
+
+    db.collection("contacts")
+      .add({
+        name: name,
+        email: email,
+        message: message,
+      })
+      .then(() => {
+        setLoader(false);
+        alert("Your message has been submitted👍");
+      })
+      .catch((error) => {
+        alert(error.message);
+        setLoader(false);
+      });
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+    <form className="form" onSubmit={handleSubmit}>
+      
+
+     
+      <input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+     
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+    
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+
+      <button
+        type="submit"
+        style={{ background: loader  }}
+      >
+        Submit
+      </button>
+      {message && <span>Thanks, I'll reply ASAP :)</span>}
+    </form>
+    </div>
+    </div>
+  );
+};
+
+export default Contact;
+
+
+
+
+
+
+
+
